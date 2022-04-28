2e7d95e0ae7fc257c0261e4ebd33b8c02314a3c7
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-04-28 09:53:52 -0300

    Configuração do componente Login

--- src/app/login-screen/login-screen-routing.module.ts
+++ src/app/login-screen/login-screen-routing.module.ts
@@ -1,11 +1,18 @@
 import { LoginScreenComponent } from './login-screen.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginScreenComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ]
   },
 ];
 
